441c2d26262b2246e68cbd645dc8bc0ab6a894fa
Archish <archish17@gmail.com>
2022-09-18 18:40:35 +0530

    updated codes

---
+++ models/architecture.py
@@ -0,0 +1,13 @@
+class Model(nn.Module):
+    def __init__(self, args):
+        super(Model, self).__init__()
+        self.args = args
+
+        self.conv = nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=kernel_size, stride=stride, padding=padding, bias=True)
+        self.fc = nn.Linear(in_features=in_features, out_features=out_features, bias=True)
+
+        self.act = nn.ReLU(inplace=True)
+
+    def forward(self, x):
+        
+        return x
